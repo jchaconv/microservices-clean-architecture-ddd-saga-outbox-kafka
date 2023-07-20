8df6f417f4f2afbbc446351ab84e2077b5700a41
Julio Chacon <jchacon.vilela@gmail.com>
2023-07-19 21:54:53 -0500

    antes de subir tests

--- food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderCreateCommandHandler.java
+++ food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderCreateCommandHandler.java
@@ -30,6 +30,6 @@ public class OrderCreateCommandHandler {
         OrderCreatedEvent orderCreatedEvent = orderCreateHelper.persistOrder(createOrderCommand);
         log.info("Order is created with id: {}", orderCreatedEvent.getOrder().getId().getValue());
         orderCreatedPaymentPublisher.publish(orderCreatedEvent);
-        return orderDataMapper.orderToCreateOrderResponse(orderCreatedEvent.getOrder());
+        return orderDataMapper.orderToCreateOrderResponse(orderCreatedEvent.getOrder(), "");
     }
 }
---
+++ food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/input/message/listener/payment/PaymentResponseListenerImpl.java
@@ -0,0 +1,24 @@
+package com.food.ordering.system.order.service.domain.ports.input.message.listener.payment;
+
+import com.food.ordering.system.order.service.domain.dto.message.PaymentResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+@Validated
+@Slf4j
+@Service
+public class PaymentResponseListenerImpl implements PaymentResponseListener {
+
+
+
+    @Override
+    public void paymentCompleted(PaymentResponse paymentResponse) {
+
+    }
+
+    @Override
+    public void paymentCancelled(PaymentResponse paymentResponse) {
+
+    }
+}
